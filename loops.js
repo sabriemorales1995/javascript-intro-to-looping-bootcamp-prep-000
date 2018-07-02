8718ef88dce41fba0d2a34c6a49ec148e1eefcac
Sabrie Morales <sabriemorales1995@gmail.com>
2018-07-02 03:02:12 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,10 @@
 function forLoop(array){
   for(var i=0; i<25;i++){
     if(i===1){
-    array= [...array, `I am ${i} strange loops.`]
+    array= [...array, `I am 1 strange loop.`];
+    }else{
+    array= [...array, `I am ${i} strange loops.`];
+    }
   }
   return array;
 }
